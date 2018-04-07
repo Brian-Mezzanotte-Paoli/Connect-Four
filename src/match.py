33b97e1e4caa472fba104b4ab3047e267257c1f4
Tommaso Paoli <paoli7612@gmail.com>
2018-04-07 23:36:04 +0200

    match

--- src/match.py
+++ src/match.py
@@ -1,6 +1,18 @@
+PLAYER, COMPUTER = 1,2
+
 class Game:
     def __init__(self):
-        pass
+        self.win = False
+
+    def change_turn(self):
+        self.turn = 3-self.turn
 
     def play(self, starter):
-        pass
+        self.turn = starter
+        while not self.win:
+            if self.turn = PLAYER:
+                pass
+            elif self.turn == COMPUTER:
+                pass
+
+            self.change_turn()
